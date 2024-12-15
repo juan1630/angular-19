1f0568dddd183a50a883129edbb8c1cbd14b739b
Juan Patrón <josejuanpatron1630@gmail.com>
2024-12-15 14:49:44 -0600

    added validation

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -10,8 +10,14 @@ import { Product } from '../../common/services/interfaces/Products.interface';
 export class StoreComponent {
 
   public productsList: Product[] =  [{product:'tele', amount: 20, id: 1}, {product: 'Iphone', amount:500, id: 2} ];
+  public isValidProduct:boolean = true;
 
   addProduct(product:string, amount:string) {
-    this.productsList.push({product, amount: Number(amount), id: 3 })
+    if(product.length <= 0 || Number(amount) <= 0){
+      console.log('Algo anda mal');
+      this.isValidProduct = false;
+    }else {
+      this.productsList.push({product, amount: Number(amount), id: 3 })
+    }
   }
 }
